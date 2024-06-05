25e0f760bd9f27cd4e9ee7d695ee6541cd53e4d7
Michael Rodriguez <est.michael.reyes@unimilitar.edu.co>
2024-06-05 11:34:47 -0500

    coments_two

--- idenpalab.py
+++ idenpalab.py
@@ -1,29 +1,29 @@
-import speech_recognition as sr
-textoanterior = "Buenos días Claro que sí antes de mostrarle las diferentes opciones que tenemos una pregunta el colchón es para usted"
+import speech_recognition as sr  # Importar la librería de reconocimiento de voz
+
+# Frase esperada
+textoanterior = "Buenos días Claro que sí antes de mostrarle las diferentes opciones que tenemos una pregunta el colchón es para usted"  # Definir la frase que se espera escuchar
 
 # Crear un objeto Recognizer
-recognizer = sr.Recognizer()
+recognizer = sr.Recognizer()  # Crear una instancia del reconocedor de voz
 
 # Utilizar el micrófono como fuente de entrada
-with sr.Microphone() as source:
-    print("Cliente: Buenos días, estoy buscando un colchon")
-    print("Amigo vendedor, dí la siguiente frase para atender a tu cliente: Buenos días, claro que si antes de mostrarle las diferentes opciones que tenemos una pregunta ¿El colchón es para usted?")
-    recognizer.adjust_for_ambient_noise(source)  # Ajustar para el ruido ambiental
-    audio = recognizer.listen(source)
+with sr.Microphone() as source:  # Utilizar el micrófono como la fuente de audio
+    print("Cliente: Buenos días, estoy buscando un colchon")  # Imprimir el texto del cliente para contexto
+    print("Amigo vendedor, dí la siguiente frase para atender a tu cliente: Buenos días, claro que si antes de mostrarle las diferentes opciones que tenemos una pregunta ¿El colchón es para usted?")  # Pedir al vendedor que diga la frase esperada
+    recognizer.adjust_for_ambient_noise(source)  # Ajustar el reconocedor para el ruido ambiental
+    audio = recognizer.listen(source)  # Escuchar y grabar el audio del micrófono
 
     try:
         # Utilizar el motor de reconocimiento de voz de Google
-        text = recognizer.recognize_google(audio, language="es-ES")
-        print("Texto reconocido: " + text)
-        if (text == textoanterior):
-           print("Correcto")
+        text = recognizer.recognize_google(audio, language="es-ES")  # Reconocer el audio usando Google y especificar el idioma español
+        print("Texto reconocido: " + text)  # Imprimir el texto reconocido
+
+        if text == textoanterior:  # Comparar el texto reconocido con el texto esperado
+            print("Correcto")  # Imprimir "Correcto" si coinciden
         else:
-            print("incorrecto")
-        
-        #print(textoanterior)
+            print("incorrecto")  # Imprimir "Incorrecto" si no coinciden
 
-    except sr.UnknownValueError:
-        print("No se pudo reconocer el audio.")
-    except sr.RequestError as e:
-        print("Error en la solicitud al motor de reconocimiento de voz; {0}".format(e))
-
+    except sr.UnknownValueError:  # Si no se puede reconocer el audio
+        print("No se pudo reconocer el audio.")  # Indicar que no se pudo reconocer el audio
+    except sr.RequestError as e:  # Si hay un error en la solicitud al motor de reconocimiento de voz
+        print("Error en la solicitud al motor de reconocimiento de voz; {0}".format(e))  # Indicar el error ocurrido
--- ventas.py
+++ ventas.py
@@ -1,17 +1,17 @@
-import spacy
-import speech_recognition as sr
-from gtts import gTTS
-import os
-import time
+import spacy  # Importar la librería de procesamiento de lenguaje natural
+import speech_recognition as sr  # Importar la librería de reconocimiento de voz
+from gtts import gTTS  # Importar la librería para convertir texto a voz
+import os  # Importar la librería para interactuar con el sistema operativo
+import time  # Importar la librería para manejar tiempos
 
 # Cargar el modelo de lenguaje en español
-nlp = spacy.load("es_core_news_sm")
+nlp = spacy.load("es_core_news_sm")  # Cargar el modelo de Spacy para español
 
 # Definir las etapas y las frases clave de la conversación
-conversacion = [
+conversacion = [  # Lista que contiene las etapas de la conversación y las frases clave asociadas a cada etapa
     {
-        "etapa": 1,
-        "frases": [
+        "etapa": 1,  # Etapa 1
+        "frases": [  # Lista de frases de la etapa 1
             {"texto": "es un gusto tenerlo en crea", "palabras_claves": ["gusto", "crea"]},
             {"texto": "mi nombre es", "palabras_claves": ["nombre"]},
             {"texto": "estoy buscando un sofá, colchón mueble", "palabras_claves": ["sofá", "colchón", "mueble"]},
@@ -22,8 +22,8 @@ conversacion = [
         ]
     },
     {
-        "etapa": 2,
-        "frases": [
+        "etapa": 2,  # Etapa 2
+        "frases": [  # Lista de frases de la etapa 2
             {"texto": "le voy a efectuar unas preguntas para determinar con exactitud el tipo de producto que está buscando y poderle asesorar mejor", "palabras_claves": ["preguntas", "para", "determinar", "exactitud", "asesorar"]},
             {"texto": "exactamente qué tipo de producto está buscando", "palabras_claves": ["exactamente"]},
             {"texto": "con qué tipo de producto en diseño se identifica normalmente", "palabras_claves": ["identifica"]},
@@ -49,56 +49,55 @@ conversacion = [
 ]
 
 # Inicializar el reconocedor de voz
-recognizer = sr.Recognizer()
+recognizer = sr.Recognizer()  # Crear una instancia del reconocedor de voz
 
 def speak(text):
-    tts = gTTS(text, lang="es")
-    tts.save("response.mp3")
-    os.system("mpg321 response.mp3")
+    tts = gTTS(text, lang="es")  # Convertir el texto a voz en español
+    tts.save("response.mp3")  # Guardar la voz generada en un archivo mp3
+    os.system("mpg321 response.mp3")  # Reproducir el archivo de audio mp3
 
 # Configurar el micrófono como fuente de entrada
-with sr.Microphone() as source:
+with sr.Microphone() as source:  # Utilizar el micrófono como la fuente de audio
     time.sleep(2)  # Agrega un retraso de 2 segundos antes de mostrar el mensaje
-    print("¡Hable con el asesor!")
+    print("¡Hable con el asesor!")  # Mensaje para iniciar la conversación
 
-    for etapa_info in conversacion:
-        etapa = etapa_info["etapa"]
-        print(f"\nEtapa {etapa}:")
+    for etapa_info in conversacion:  # Iterar a través de cada etapa de la conversación
+        etapa = etapa_info["etapa"]  # Obtener el número de la etapa
+        print(f"\nEtapa {etapa}:")  # Imprimir el número de la etapa
 
-        for i, frase_info in enumerate(etapa_info["frases"]):
-            frase = frase_info["texto"]
-            palabras_claves = frase_info["palabras_claves"]
+        for i, frase_info in enumerate(etapa_info["frases"]):  # Iterar a través de cada frase de la etapa
+            frase = frase_info["texto"]  # Obtener el texto de la frase
+            palabras_claves = frase_info["palabras_claves"]  # Obtener las palabras clave de la frase
 
-            print(f"\nFrase {i + 1}: {frase}")
-            print(f"Palabras claves: {', '.join(palabras_claves)}")
+            print(f"\nFrase {i + 1}: {frase}")  # Imprimir el número y el texto de la frase
+            print(f"Palabras claves: {', '.join(palabras_claves)}")  # Imprimir las palabras clave
 
             input("Presione Enter para continuar...")  # Espera a que el usuario presione Enter antes de continuar
-            speak(frase)
+            speak(frase)  # Reproducir la frase usando texto a voz
             
             while True:
-                audio = recognizer.listen(source)
+                audio = recognizer.listen(source)  # Escuchar y grabar el audio del micrófono
 
                 try:
-                    respuesta_asesor = recognizer.recognize_google(audio, language="es-ES")
-                    print("Asesor: " + respuesta_asesor)
+                    respuesta_asesor = recognizer.recognize_google(audio, language="es-ES")  # Reconocer el audio usando Google y especificar el idioma español
+                    print("Asesor: " + respuesta_asesor)  # Imprimir el texto reconocido
 
-                    tokens_respuesta = nlp(respuesta_asesor)
-                    tokens_frase = nlp(frase)
-                    palabras_clave_encontradas = [token.text for token in tokens_respuesta if token.text in palabras_claves]
+                    tokens_respuesta = nlp(respuesta_asesor)  # Procesar el texto reconocido con Spacy
+                    tokens_frase = nlp(frase)  # Procesar el texto de la frase con Spacy
+                    palabras_clave_encontradas = [token.text for token in tokens_respuesta if token.text in palabras_claves]  # Encontrar las palabras clave en la respuesta
 
-                    porcentaje_asertividad = (len(palabras_clave_encontradas) / len(palabras_claves)) * 100
+                    porcentaje_asertividad = (len(palabras_clave_encontradas) / len(palabras_claves)) * 100  # Calcular el porcentaje de palabras clave encontradas
 
-                    print(f"Porcentaje de asertividad en la etapa {etapa}, frase {i + 1}: {porcentaje_asertividad}%")
+                    print(f"Porcentaje de asertividad en la etapa {etapa}, frase {i + 1}: {porcentaje_asertividad}%")  # Imprimir el porcentaje de asertividad
 
-                    if porcentaje_asertividad >= 75 and all(palabra_clave in respuesta_asesor for palabra_clave in palabras_claves):
-                        break
+                    if porcentaje_asertividad >= 75 and all(palabra_clave in respuesta_asesor for palabra_clave in palabras_claves):  # Si el porcentaje de asertividad es mayor o igual al 75% y todas las palabras clave están en la respuesta
+                        break  # Salir del bucle y pasar a la siguiente frase
                     else:
-                        print("El asesor no cumplió con la asertividad o no dijo la palabra clave en esta etapa. Repitiendo la frase.")
+                        print("El asesor no cumplió con la asertividad o no dijo la palabra clave en esta etapa. Repitiendo la frase.")  # Indicar que no se cumplió con la asertividad y repetir la frase
 
-                except sr.UnknownValueError:
-                    print("No se pudo reconocer el audio.")
-                except sr.RequestError as e:
-                    print(f"Error en la solicitud al motor de reconocimiento de voz; {e}")
+                except sr.UnknownValueError:  # Si no se puede reconocer el audio
+                    print("No se pudo reconocer el audio.")  # Indicar que no se pudo reconocer el audio
+                except sr.RequestError as e:  # Si hay un error en la solicitud al motor de reconocimiento de voz
+                    print(f"Error en la solicitud al motor de reconocimiento de voz; {e}")  # Indicar el error ocurrido
 
-print("Fin de la conversación.")
-
+print("Fin de la conversación.")  # Mensaje al finalizar la conversación
